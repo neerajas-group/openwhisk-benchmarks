d95becb8c201cf8eb9344b5a1727a78a9d16e86a
Chameleon Cloud User <cc@psinhaow-1.novalocal>
2022-12-13 22:50:58 +0000

    Move data and update scripts slightly

--- runtests/matmult/matmult-test.sh
+++ runtests/matmult/matmult-test.sh
@@ -1,15 +1,15 @@
 # !/bin/bash
 
 # CPU: 16, Memory: 2GB
-# ./run-matmult.sh 16 2048 8000 >> matmult-16-2048-8000.txt
-# ./run-matmult.sh 16 2048 7000 >> matmult-16-2048-7000.txt
-# ./run-matmult.sh 16 2048 6000 >> matmult-16-2048-6000.txt
-# ./run-matmult.sh 16 2048 5000 >> matmult-16-2048-5000.txt
-# ./run-matmult.sh 16 2048 4000 >> matmult-16-2048-4000.txt
-# ./run-matmult.sh 16 2048 3000 >> matmult-16-2048-3000.txt
-# ./run-matmult.sh 16 2048 2000 >> matmult-16-2048-2000.txt
-# ./run-matmult.sh 16 2048 1000 >> matmult-16-2048-1000.txt
-# ./run-matmult.sh 16 2048 500 >> matmult-16-2048-500.txt
+./run-matmult.sh 16 2048 8000 >> matmult-16-2048-8000.txt
+./run-matmult.sh 16 2048 7000 >> matmult-16-2048-7000.txt
+./run-matmult.sh 16 2048 6000 >> matmult-16-2048-6000.txt
+./run-matmult.sh 16 2048 5000 >> matmult-16-2048-5000.txt
+./run-matmult.sh 16 2048 4000 >> matmult-16-2048-4000.txt
+./run-matmult.sh 16 2048 3000 >> matmult-16-2048-3000.txt
+./run-matmult.sh 16 2048 2000 >> matmult-16-2048-2000.txt
+./run-matmult.sh 16 2048 1000 >> matmult-16-2048-1000.txt
+./run-matmult.sh 16 2048 500 >> matmult-16-2048-500.txt
 
 # CPU: 32, Memory: 2GB
 ./run-matmult.sh 32 2048 500 >> matmult-32-2048-500.txt
--- runtests/matmult/run-matmult.sh
+++ runtests/matmult/run-matmult.sh
@@ -12,6 +12,7 @@ cd ~/openwhisk-benchmarks/functions/matmult
 CPU=$1
 MEM=$2
 MAT_SIZE=$3
+MACHINE="129.114.109.253"
 
 # Register matrix multiplication function with our OpenWhisk setup
 echo "Experiment Setup:"
@@ -25,10 +26,10 @@ fi
 wsk -i action $ACTION_OP $FXN $FXN.py --docker psinha25/main-python --web raw --memory $MEM --cpu $CPU
 
 # Start up the faas profiler running other 4 functions as noise
-cd ~/faas-profiler
-./WorkloadInvoker -c ~/openwhisk-benchmarks/runtests/matmult/matmult-config.json &
-sleep 10
-echo ""
+# cd ~/faas-profiler
+# ./WorkloadInvoker -c ~/openwhisk-benchmarks/runtests/matmult/matmult-config.json &
+# sleep 10
+# echo ""
 
 echo "Experiment Details:"
 echo "-------------------"
@@ -41,12 +42,12 @@ echo "Experiment Begins:"
 echo "------------------"
 for size in $MAT_SIZE; do 
     echo "MATRIX SIZE: $size"
-    STATS_PID=$(ssh 129.114.109.238 " python3 ~/stats-collector.py >> docker-stats-$CPU-$MEM-$MAT_SIZE-${FXN}.txt & jobs -p")
-
+    STATS_PID=$(ssh $MACHINE " python3 -u ~/stats-collector.py >> docker-stats-$CPU-$MEM-$MAT_SIZE-${FXN}.txt & jobs -p")
+    
     # Do 10 runs per matrix
     for run in {1..10} ; do
 
-        # 1. Get latency of critical section of function
+        # Get latency of critical section of function
         LAT_LINE=$(wsk action invoke matmult -i -p N ${size} -r | grep "latency")
         # STREAM_PID=$(ssh 129.114.109.238 " ps -aux | grep \"while true; do docker stats --no-stream\" | cut -d ' ' -f8 ")
         # ssh 129.114.109.238 " kill $STREAM_PID "
@@ -54,20 +55,20 @@ for size in $MAT_SIZE; do
         LATENCY=${L_LINE[1]}
         echo "latency: $LATENCY"
 
-        # 2. Collect max memory usage
-        CONTAINER_LINE=$(ssh 129.114.109.238 " docker ps --no-trunc | grep main-python ")
+        # Collect max memory usage
+        CONTAINER_LINE=$(ssh $MACHINE " docker ps --no-trunc | grep main-python ")
         C_LINE=($CONTAINER_LINE)
         CONTAINER_ID=${C_LINE[0]}
-        MAX_MEM=$(ssh 129.114.109.238 "cat /sys/fs/cgroup/memory/docker/$CONTAINER_ID/memory.max_usage_in_bytes")
+        MAX_MEM=$(ssh $MACHINE "cat /sys/fs/cgroup/memory/docker/$CONTAINER_ID/memory.max_usage_in_bytes")
         echo "max_mem: $MAX_MEM"
         sleep 5
 
     done
 
-    # STATS_PID=$(ssh 129.114.109.238 " ps -aux | grep \"python3 ~/stats-collector.py\" | cut -d ' ' -f8 ")
-    ssh 129.114.109.238 " kill $STATS_PID "
+    # STATS_PID=$(ssh $MACHINE " ps -aux | grep \"python3 ~/stats-collector.py\" | cut -d ' ' -f8 ")
+    ssh $MACHINE " kill $STATS_PID "
 
 done
 
-# Stop all container created by FaasProfiler
-ssh 129.114.109.238 " docker ps | grep psinha25 | cut -d ' ' -f1 | xargs -I {} docker stop -t 1 {} "
+# Stop all containers created FaasProfiler & the container of interest
+ssh $MACHINE " docker ps | grep psinha25 | cut -d ' ' -f1 | xargs -I {} docker stop -t 1 {} "
